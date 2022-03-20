04d22eaa9da376329abfcbc2ceca6eb332b01d81
kadirnar <kadir.nar@hotmail.com>
2022-03-21 01:41:15 +0300

    train update

---
+++ semantic_train.py
@@ -0,0 +1,231 @@
+from pprint import pprint
+import segmentation_models_pytorch as smp
+from metrics.functional import get_stats, iou_score
+from oxford_pet import SimpleOxfordPetDataset
+from catalyst import utils
+import os
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+
+# Setting up GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # "" - CPU, "0" - 1 GPU, "0,1" - MultiGPU
+SEED = 42
+utils.set_global_seed(SEED)
+utils.prepare_cudnn(deterministic=True)
+
+# Datasets
+root = "."
+SimpleOxfordPetDataset.download(root)
+
+# init train, val, test sets
+train_dataset = SimpleOxfordPetDataset(root, "train")
+valid_dataset = SimpleOxfordPetDataset(root, "valid")
+test_dataset = SimpleOxfordPetDataset(root, "test")
+
+# It is a good practice to check datasets don`t intersects with each other
+assert set(test_dataset.filenames).isdisjoint(set(train_dataset.filenames))
+assert set(test_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+assert set(train_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+
+print(f"Train size: {len(train_dataset)}")
+print(f"Valid size: {len(valid_dataset)}")
+print(f"Test size: {len(test_dataset)}")
+
+n_cpu = os.cpu_count()
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=n_cpu)
+valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+
+# lets look at some samples
+sample = train_dataset[0]
+plt.subplot(1, 2, 1)
+plt.imshow(sample["image"].transpose(1, 2, 0))  # for visualization we have to transpose back to HWC
+plt.subplot(1, 2, 2)
+plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+# plt.show()
+
+sample = valid_dataset[0]
+plt.subplot(1, 2, 1)
+plt.imshow(sample["image"].transpose(1, 2, 0))  # for visualization we have to transpose back to HWC
+plt.subplot(1, 2, 2)
+plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+# plt.show()
+
+sample = test_dataset[0]
+plt.subplot(1, 2, 1)
+plt.imshow(sample["image"].transpose(1, 2, 0))  # for visualization we have to transpose back to HWC
+plt.subplot(1, 2, 2)
+plt.imshow(sample["mask"].squeeze())  # for visualization we have to remove 3rd dimension of mask
+
+
+# plt.show()
+
+# Create segmentation model
+
+class PetModel(pl.LightningModule):
+
+    def __init__(self, arch, encoder_name, in_channels, out_classes, **kwargs):
+        super().__init__()
+        self.model = smp.create_model(
+            arch, encoder_name=encoder_name, in_channels=in_channels, classes=out_classes, **kwargs
+        )
+
+        # preprocessing parameteres for image
+        params = smp.encoders.get_preprocessing_params(encoder_name)
+        self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
+        self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
+
+        # for image segmentation dice loss could be the best first choice
+        self.loss_fn = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+
+    def forward(self, image):
+        # normalize image here
+        image = (image - self.mean) / self.std
+        mask = self.model(image)
+        return mask
+
+    def shared_step(self, batch, stage):
+        image = batch["image"]
+
+        # Shape of the image should be (batch_size, num_channels, height, width)
+        # if you work with grayscale images, expand channels dim to have [batch_size, 1, height, width]
+        assert image.ndim == 4
+
+        # Check that image dimensions are divisible by 32,
+        # encoder and decoder connected by `skip connections` and usually encoder have 5 stages of
+        # downsampling by factor 2 (2 ^ 5 = 32); e.g. if we have image with shape 65x65 we will have
+        # following shapes of features in encoder and decoder: 84, 42, 21, 10, 5 -> 5, 10, 20, 40, 80
+        # and we will get an error trying to concat these features
+        h, w = image.shape[2:]
+        assert h % 32 == 0 and w % 32 == 0
+
+        mask = batch["mask"]
+
+        # Shape of the mask should be [batch_size, num_classes, height, width]
+        # for binary segmentation num_classes = 1
+        assert mask.ndim == 4
+
+        # Check that mask values in between 0 and 1, NOT 0 and 255 for binary segmentation
+        assert mask.max() <= 1.0 and mask.min() >= 0
+
+        logits_mask = self.forward(image)
+
+        # Predicted mask contains logits, and loss_fn param `from_logits` is set to True
+        loss = self.loss_fn(logits_mask, mask)
+
+        # Lets compute metrics for some threshold
+        # first convert mask values to probabilities, then
+        # apply thresholding
+        prob_mask = logits_mask.sigmoid()
+        pred_mask = (prob_mask > 0.5).float()
+
+        # We will compute IoU metric by two ways
+        #   1. dataset-wise
+        #   2. image-wise
+        # but for now we just compute true positive, false positive, false negative and
+        # true negative 'pixels' for each image and class
+        # these values will be aggregated in the end of an epoch
+        tp, fp, fn, tn = get_stats(pred_mask.long(), mask.long(), mode="binary")
+
+        return {
+            "loss": loss,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+        }
+
+    def shared_epoch_end(self, outputs, stage):
+        # aggregate step metics
+        tp = torch.cat([x["tp"] for x in outputs])
+        fp = torch.cat([x["fp"] for x in outputs])
+        fn = torch.cat([x["fn"] for x in outputs])
+        tn = torch.cat([x["tn"] for x in outputs])
+
+        # per image IoU means that we first calculate IoU score for each image
+        # and then compute mean over these scores
+        per_image_iou = iou_score(tp, fp, fn, tn, reduction="micro-imagewise")
+
+        # dataset IoU means that we aggregate intersection and union over whole dataset
+        # and then compute IoU score. The difference between dataset_iou and per_image_iou scores
+        # in this particular case will not be much, however for dataset
+        # with "empty" images (images without target class) a large gap could be observed.
+        # Empty images influence a lot on per_image_iou and much less on dataset_iou.
+        dataset_iou = iou_score(tp, fp, fn, tn, reduction="micro")
+
+        metrics = {
+            f"{stage}_per_image_iou": per_image_iou,
+            f"{stage}_dataset_iou": dataset_iou,
+        }
+
+        self.log_dict(metrics, prog_bar=True)
+
+    def training_step(self, batch, batch_idx):
+        return self.shared_step(batch, "train")
+
+    def training_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self.shared_step(batch, "valid")
+
+    def validation_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "valid")
+
+    def test_step(self, batch, batch_idx):
+        return self.shared_step(batch, "test")
+
+    def test_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "test")
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.0001)
+
+
+model = PetModel("FPN", "resnet34", in_channels=3, out_classes=1)
+trainer = pl.Trainer(
+    gpus=1,
+    max_epochs=5,
+)
+
+trainer.fit(
+    model,
+    train_dataloaders=train_dataloader,
+    val_dataloaders=valid_dataloader,
+)
+
+# run validation dataset
+valid_metrics = trainer.validate(model, dataloaders=valid_dataloader, verbose=False)
+pprint(valid_metrics)
+
+# run test dataset
+test_metrics = trainer.test(model, dataloaders=test_dataloader, verbose=False)
+pprint(test_metrics)
+
+batch = next(iter(test_dataloader))
+with torch.no_grad():
+    model.eval()
+    logits = model(batch["image"])
+pr_masks = logits.sigmoid()
+
+for image, gt_mask, pr_mask in zip(batch["image"], batch["mask"], pr_masks):
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(image.numpy().transpose(1, 2, 0))  # convert CHW -> HWC
+    plt.title("Image")
+    plt.axis("off")
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(gt_mask.numpy().squeeze())  # just squeeze classes dim, because we have only one class
+    plt.title("Ground truth")
+    plt.axis("off")
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(pr_mask.numpy().squeeze())  # just squeeze classes dim, because we have only one class
+    plt.title("Prediction")
+    plt.axis("off")
+
+    plt.show()
